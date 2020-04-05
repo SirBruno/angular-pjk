b6c4b60f88e72305c0759dab64be9826f42905d6
Bruno <bruno.webjr@gmail.com>
2020-04-05 15:56:26 -0300

    added a priority field

--- src/app/tasks.ts
+++ src/app/tasks.ts
@@ -1,17 +1,20 @@
-export const tasks: {name: string, description: string, id: number}[] = [
+export const tasks: {name: string, description: string, id: number, priority: 'low' | 'medium' | 'high'}[] = [
     {
         id: 0,
         name: 'clean my room',
-        description: 'haven\'t cleaned this place in a while'
+        description: 'haven\'t cleaned this place in a while',
+        priority: 'high'
     },
     {   
         id: 1,
         name: 'jump rope',
-        description: 'i\'m trying to lose weight and get fit'
+        description: 'i\'m trying to lose weight and get fit',
+        priority: 'low'
     },
     {
         id: 2,
         name: 'drink coffee',
-        description: 'can\'t focus without coffee'
+        description: 'can\'t focus without coffee',
+        priority: 'medium'
     }
 ]
